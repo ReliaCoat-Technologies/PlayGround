0525b6553136f6ddba59feda1cf9c09865139450
asagiv <ari.sagiv@reliacoat.com>
2020-01-21 10:13:16 -0500

    Add sample box annotation creator, begin implementation of rotate transforms.

--- DXApplication1/SciChartAnnotationsExperiments/MainWindow.xaml.cs
+++ DXApplication1/SciChartAnnotationsExperiments/MainWindow.xaml.cs
@@ -1,28 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DevExpress.Xpf.Bars;
 using DevExpress.Xpf.Core;
+using SciChartAnnotationsExperiments.ViewModels;
+using SciChart.Charting.Visuals.Annotations;
 
 namespace SciChartAnnotationsExperiments
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : ThemedWindow
     {
+        #region Fields
+        private readonly MainWindowViewModel _viewModel;
+        #endregion
+
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+
+            _viewModel = new MainWindowViewModel();
+            DataContext = _viewModel;
         }
+        #endregion
+
+        #region Methods
+        private void onBoxDrawClicked(object sender, ItemClickEventArgs e)
+        {
+            if (boxCheckItem.IsChecked == true)
+            {
+                enableBoxDraw();
+            }
+            else
+            {
+                disableBoxDraw();
+            }
+        }
+
+        private void enableBoxDraw()
+        {
+            _viewModel.rubberBandModifier.IsEnabled = false;
+            _viewModel.annotationCreationModifier.IsEnabled = true;
+            _viewModel.annotationCreationModifier.AnnotationCreated += onAnnotationCreated;
+        }
+
+        private void disableBoxDraw()
+        {
+            _viewModel.annotationCreationModifier.IsEnabled = false;
+            _viewModel.annotationCreationModifier.AnnotationCreated -= onAnnotationCreated;
+            _viewModel.rubberBandModifier.IsEnabled = true;
+        }
+
+        private void onAnnotationCreated(object sender, SciChart.Charting.ChartModifiers.AnnotationCreationArgs e)
+        {
+            var annotation = (BoxAnnotation)_viewModel.annotationCreationModifier.Annotation;
+
+            annotation.IsEditable = true;
+            annotation.LayoutTransform= new RotateTransform(-45, 0.5, 0.5);
+
+            disableBoxDraw();
+            boxCheckItem.IsChecked = false;
+        }
+        #endregion
     }
 }
--- DXApplication1/SciChartAnnotationsExperiments/ViewModels/MainWindowViewModel.cs
+++ DXApplication1/SciChartAnnotationsExperiments/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,139 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Annotations;
+using System.Windows.Media;
 using DevExpress.Mvvm;
+using SciChart.Charting.ChartModifiers;
+using SciChart.Charting.Model.ChartSeries;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Charting.Visuals.Annotations;
+using SciChart.Charting.Visuals.Axes;
+using SciChart.Data.Model;
 
 namespace SciChartAnnotationsExperiments.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        #region Fields
+        private ObservableCollection<IRenderableSeriesViewModel> _renderableSeriesList;
+        private IAxis _xAxis;
+        private IAxis _yAxis;
+        private AnnotationCollection _annotations;
+        private ModifierGroup _modifiers;
+        private RubberBandXyZoomModifier _rubberBandModifier;
+        private AnnotationCreationModifier _annotationCreationModifier;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<IRenderableSeriesViewModel> renderableSeriesList
+        {
+            get { return _renderableSeriesList; }
+            set { _renderableSeriesList = value; RaisePropertyChanged(() => renderableSeriesList); }
+        }
+        public IAxis xAxis
+        {
+            get { return _xAxis; }
+            set { _xAxis = value; RaisePropertyChanged(() => xAxis); }
+        }
+        public IAxis yAxis
+        {
+            get { return _yAxis; }
+            set { _yAxis = value; RaisePropertyChanged(() => yAxis); }
+        }
+        public AnnotationCollection annotations
+        {
+            get { return _annotations; }
+            set { _annotations = value; RaisePropertyChanged(() => annotations); }
+        }
+        public ModifierGroup modifiers
+        {
+            get { return _modifiers; }
+            set { _modifiers = value; RaisePropertyChanged(() => modifiers); }
+        }
+        public RubberBandXyZoomModifier rubberBandModifier
+        {
+            get { return _rubberBandModifier; }
+            set { _rubberBandModifier = value; RaisePropertyChanged(() => rubberBandModifier); }
+        }
+        public AnnotationCreationModifier annotationCreationModifier
+        {
+            get { return _annotationCreationModifier; }
+            set { _annotationCreationModifier = value; RaisePropertyChanged(() => annotationCreationModifier);}
+        }
+        #endregion
+
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            renderableSeriesList = new ObservableCollection<IRenderableSeriesViewModel>();
+
+            modifiers = new ModifierGroup();
+            annotations = new AnnotationCollection();
+            annotationCreationModifier = new AnnotationCreationModifier();
+
+            setAxesAndModifiers();
+            plotData();
+        }
+
+        private void setAxesAndModifiers()
+        {
+            xAxis = new NumericAxis
+            {
+                AxisTitle = "X",
+                DrawMajorBands = false,
+                AutoRange = AutoRange.Once
+            };
+
+            yAxis = new NumericAxis
+            {
+                AxisTitle = "Y",
+                GrowBy = new DoubleRange(0.1, 0.1),
+                DrawMajorBands = false,
+                AutoRange = AutoRange.Once
+            };
+
+            annotationCreationModifier = new AnnotationCreationModifier
+            {
+                ExecuteOn = ExecuteOn.MouseLeftButton,
+                AnnotationType = typeof(BoxAnnotation),
+                IsEnabled = false
+            };
+
+            rubberBandModifier = new RubberBandXyZoomModifier();
+
+            var zoomExtentsModifier = new ZoomExtentsModifier
+            {
+                ExecuteOn = ExecuteOn.MouseDoubleClick
+            };
+
+            modifiers.ChildModifiers.Add(zoomExtentsModifier);
+            modifiers.ChildModifiers.Add(rubberBandModifier);
+            modifiers.ChildModifiers.Add(annotationCreationModifier);
+        }
+
+        public void plotData()
+        {
+            var xValues = Enumerable.Range(-50000, 100001)
+                .Select(x => x / 5000d)
+                .ToList();
+
+            var yValues = xValues
+                .Select(Math.Cos)
+                .ToList();
+
+            var dataSeries = new XyDataSeries<double, double>();
+            dataSeries.Append(xValues, yValues);
+
+            var renderableSeries = new LineRenderableSeriesViewModel
+            {
+                Stroke = Colors.DodgerBlue,
+                DataSeries = dataSeries
+            };
+
+            renderableSeriesList.Add(renderableSeries);
+        }
+        #endregion
+
     }
 }

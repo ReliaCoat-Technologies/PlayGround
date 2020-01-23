402b1665de81eff424d31b4be80e661f6ab2ec5f
asagiv <ari.sagiv@reliacoat.com>
2020-01-23 15:26:15 -0500

    Adjust nomenclature, documentation.

--- DXApplication1/SciChartAnnotationsExperiments/CustomAnnotations/EllipseAnnotation.xaml.cs
+++ DXApplication1/SciChartAnnotationsExperiments/CustomAnnotations/EllipseAnnotation.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using SciChart.Charting.Visuals.Annotations;
 
@@ -7,11 +8,13 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
     public partial class EllipseAnnotation : BoxAnnotation
     {
         #region
-        private ModifiableEllipse _modifiableEllipse;
+        // Untranslated annotation dimensions.
         private double _x1;
         private double _x2;
         private double _y1;
         private double _y2;
+
+        private ModifiableEllipse _modifiableEllipse;
         private readonly AnnotationTransformContext _transformContext;
         #endregion
 
@@ -20,7 +23,9 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
         {
             InitializeComponent();
             Tag = "Drawn Annotation";
+            ClipToBounds = false;
             IsEditable = true;
+            IsResizable = true;
 
             _transformContext = new AnnotationTransformContext();
         }
@@ -29,28 +34,41 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
         #region Methods
         private void onEllipseLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
+            // Sets modifiable ellipse property from XAML template upon runtime loading.
             _modifiableEllipse = sender as ModifiableEllipse;
 
             if (_modifiableEllipse == null) return;
 
             SizeChanged += (s, e) => _modifiableEllipse.onParentSizeChanged(e.NewSize);
-
+            
             DragStarted += onDragStart;
-            DragEnded += OnDragEnded;
-            _modifiableEllipse.revertEllipseSize += revertBoxSize;
-            _modifiableEllipse.ellipseRendered += renderModifiedEllipse;
+            DragEnded += onDragEnded;
+
+            _modifiableEllipse.rotationStarted += revertBoxSize;
+            _modifiableEllipse.rotationCompleted += renderModifiedEllipse;
         }
 
         private void onDragStart(object sender, EventArgs e)
         {
-            Console.WriteLine($"Drag Start: [{X1} {X2} {Y1} {Y2}]");
+            // Occurs original dimensions have not yet been set.
+            if (new[] { _x1, _y1, _x2, _y2 }.All(x => x == 0))
+            {
+                _x1 = Convert.ToDouble(X1);
+                _x2 = Convert.ToDouble(X2);
+                _y1 = Convert.ToDouble(Y1);
+                _y2 = Convert.ToDouble(Y2);
+            }
+
+            // Takes down bounds before transforming.
             _transformContext.setBeforeTransform(X1, X2, Y1, Y2);
         }
 
-        private void OnDragEnded(object sender, EventArgs e)
+        private void onDragEnded(object sender, EventArgs e)
         {
-            Console.WriteLine($"Drag End: [{X1} {X2} {Y1} {Y2}]");
+            // Takes down bounds after transforming.
             _transformContext.setAfterTransform(X1, X2, Y1, Y2);
+
+            // Calculates transformation parameters.
             _transformContext.calculateTransformValues();
 
             Console.WriteLine($"Shift: Horizontal:{_transformContext.deltaXMidpoint}, Vertical:{_transformContext.deltaYMidpoint}");
@@ -61,24 +79,22 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
             var adjustedWidth = adjustedOriginDimensions[0];
             var adjustedHeight = adjustedOriginDimensions[1];
 
-            // _x1 += _transformContext.deltaXMidpoint;
-            // _x2 += _transformContext.deltaXMidpoint;
-            // _y1 += _transformContext.deltaYMidpoint;
-            // _y2 += _transformContext.deltaYMidpoint;
-
             var _xMidpoint = (_x1 + _x2 + 2 * _transformContext.deltaXMidpoint) / 2;
             var _yMidpoint = (_y1 + _y2 + 2 * _transformContext.deltaYMidpoint) / 2;
 
             _x1 = _xMidpoint - adjustedWidth / 2;
             _x2 = _xMidpoint + adjustedWidth / 2;
-            _y1 = _yMidpoint + adjustedHeight / 2;
-            _y2 = _yMidpoint - adjustedHeight / 2;
+            _y1 = _yMidpoint - adjustedHeight / 2;
+            _y2 = _yMidpoint + adjustedHeight / 2;
+
+            Console.WriteLine($"Original Transform: [X1:{_x1} X2:{_x2} Y1:{_y1} Y2:{_y2}]");
         }
 
         private void revertBoxSize()
         {
-            if (_x1 == 0) return;
+            if (new[] { _x1, _y1, _x2, _y2 }.All(x => x == 0)) return;
 
+            // Sets box size to un-transformed ellipse size for rotation.
             X1 = _x1;
             X2 = _x2;
             Y1 = _y1;
@@ -87,20 +103,23 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
 
         private void renderModifiedEllipse(double widthRatio, double heightRatio)
         {
+            // Stores the un-transformed ellipses bounds
             _x1 = Convert.ToDouble(X1);
             _x2 = Convert.ToDouble(X2);
             _y1 = Convert.ToDouble(Y1);
             _y2 = Convert.ToDouble(Y2);
 
+            // Transformation is centered around the ellipse's midpoint
             var xMidpoint = (_x2 + _x1) / 2;
             var yMidpoint = (_y2 + _y1) / 2;
-
+            
             var xHalfRange = Math.Abs((_x2 - _x1) / 2);
             var yHalfRange = Math.Abs((_y2 - _y1) / 2);
 
             var adjustedXHalfRange = xHalfRange * widthRatio;
             var adjustedYHalfRange = yHalfRange * heightRatio;
 
+            // Applying the adjusted ellipse's bound corners.
             X1 = xMidpoint - adjustedXHalfRange;
             X2 = xMidpoint + adjustedXHalfRange;
             Y1 = yMidpoint - adjustedYHalfRange;
--- DXApplication1/SciChartAnnotationsExperiments/CustomAnnotations/ModifiableEllipse.xaml.cs
+++ DXApplication1/SciChartAnnotationsExperiments/CustomAnnotations/ModifiableEllipse.xaml.cs
@@ -3,24 +3,21 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using DevExpress.Xpf.Core;
 
 namespace SciChartAnnotationsExperiments.CustomAnnotations
 {
     public partial class ModifiableEllipse : UserControl
     {
         #region Fields
-        private bool _dragAction;
+        private readonly RotateTransform _rotateTransform;
+
+        private bool _isDragging;
         private Point _ellipseOrigin;
-        private Vector _originalVector;
-        private double _originalAngle;
-        private RotateTransform _rotateTransform;
-        private bool _isTransformed => ellipse.Visibility != Visibility.Visible;
         #endregion
 
         #region Delegates
-        public event Action revertEllipseSize;
-        public event Action<double, double> ellipseRendered;
+        public event Action rotationStarted;
+        public event Action<double, double> rotationCompleted;
         #endregion
 
         #region Constructor
@@ -36,6 +33,7 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
             ellipse.RenderTransformOrigin = new Point(0.5, 0.5);
             ellipse.RenderTransform = _rotateTransform;
 
+            // Showing the original ellipse when it's being created.
             ellipse.Visibility = Visibility.Visible;
             renderedPath.Visibility = Visibility.Hidden;
             angleLine.Visibility = Visibility.Hidden;
@@ -45,6 +43,7 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
         #region Methods
         public void onParentSizeChanged(Size e)
         {
+            // Resets the ellipse origin when the size has changed.
             _ellipseOrigin = new Point(e.Width / 2, e.Height / 2);
         }
 
@@ -53,23 +52,24 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
             MouseMove += updateEllipseAngle;
             MouseRightButtonUp += onRightButtonUp;
 
-            revertEllipseSize?.Invoke();
+            rotationStarted?.Invoke();
 
             ellipse.Visibility = Visibility.Visible;
             renderedPath.Visibility = Visibility.Hidden;
             angleLine.Visibility = Visibility.Visible;
 
-            _dragAction = true;
+            _isDragging = true;
         }
 
         private void updateEllipseAngle(object sender, MouseEventArgs e)
         {
-            if (!_dragAction) return;
+            if (!_isDragging) return;
 
             if (Mouse.RightButton != MouseButtonState.Pressed) return;
 
             var mousePosition = e.GetPosition(border);
 
+            // Calculates the vector of the mouse with respect to the ellipse's origin.
             angleLine.X1 = _ellipseOrigin.X;
             angleLine.Y1 = _ellipseOrigin.Y;
             angleLine.X2 = mousePosition.X;
@@ -78,44 +78,46 @@ namespace SciChartAnnotationsExperiments.CustomAnnotations
             var deltaX = mousePosition.X - _ellipseOrigin.X;
             var deltaY = mousePosition.Y - _ellipseOrigin.Y;
 
+            // Calculates the angle of the vector with respect to the X-Axis, in degrees.
             var angle = Math.Atan(deltaY / deltaX) * 180 / Math.PI;
 
+            // Sets the Rotate Transform's angle appropriately.
             _rotateTransform.Angle = angle;
         }
 
         private void onRightButtonUp(object sender, MouseButtonEventArgs e)
         {
-            _dragAction = false;
+            _isDragging = false;
 
             MouseMove -= updateEllipseAngle;
             MouseRightButtonUp -= onRightButtonUp;
 
-            if (_isTransformed) return;
-
+            // Converts WPF Ellipse to WPF Path
             var matrix = _rotateTransform.Value;
 
-            // Does a true transform of the shape.
+            // Gets the Geometry of the un-transformed and its bounds.
             var ellipseGeometry = ellipse.RenderedGeometry;
-
             var originalBounds = ellipseGeometry.Bounds;
 
+            // Performs transformation on the ellipse's geometry and creates a path based on it.
             ellipseGeometry.Transform = new MatrixTransform(matrix);
             var transformedEllipsePath = ellipseGeometry.GetOutlinedPathGeometry();
-
-            ellipse.Visibility = Visibility.Hidden;
             renderedPath.Data = transformedEllipsePath;
+
+            // Obtains the bounds of the transformed ellipse's path
+            var transformedBounds = renderedPath.Data.Bounds;
+
+            // Hides the original ellipse, shows the transformed path instead.
+            ellipse.Visibility = Visibility.Hidden;
             renderedPath.Visibility = Visibility.Visible;
             angleLine.Visibility = Visibility.Hidden;
 
-            var renderedBounds = renderedPath.Data.Bounds;
+            // Obtains change in width and height of ellipse after rotation.
+            var widthRatio = transformedBounds.Width / originalBounds.Width;
+            var heightRatio = transformedBounds.Height / originalBounds.Height;
 
-            var widthRatio = renderedBounds.Width / originalBounds.Width;
-            var heightRatio = renderedBounds.Height / originalBounds.Height;
-
-            ellipseRendered?.Invoke(widthRatio, heightRatio);
+            rotationCompleted?.Invoke(widthRatio, heightRatio);
         }
         #endregion
-
-
     }
 }

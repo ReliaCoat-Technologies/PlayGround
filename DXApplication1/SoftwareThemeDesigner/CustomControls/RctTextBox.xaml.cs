19b65453df134a89f7e1118c8f2b4a935caa1970
Ari <DonSagiv@users.noreply.github.com>
2020-02-14 13:25:31 -0500

    Software Theme Developer - Custom Text Box.

---
+++ DXApplication1/SoftwareThemeDesigner/CustomControls/RctTextBox.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Controls;
+using DevExpress.Xpf.Editors;
+
+namespace SoftwareThemeDesigner.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for RctTextBox.xaml
+    /// </summary>
+    public partial class RctTextBox : TextEdit
+    {
+        public RctTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}

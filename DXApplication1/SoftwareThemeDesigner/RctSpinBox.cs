a8654f17c36b8588fd78e0907c96696866c61fee
Ari <DonSagiv@users.noreply.github.com>
2020-02-20 16:50:52 -0500

    Add SpinBox custom control.

---
+++ DXApplication1/SoftwareThemeDesigner/RctSpinBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SoftwareThemeDesigner
+{
+	public class RctSpinBox : RctTextBox
+	{
+		static RctSpinBox()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(RctSpinBox), new FrameworkPropertyMetadata(typeof(RctSpinBox)));
+		}
+	}
+}

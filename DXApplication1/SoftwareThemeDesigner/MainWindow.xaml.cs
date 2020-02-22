b16f33693324451a827faf1e562c6f73a2006f4b
Ari <DonSagiv@users.noreply.github.com>
2020-02-22 08:27:46 -0500

    Add password box.

--- DXApplication1/SoftwareThemeDesigner/MainWindow.xaml.cs
+++ DXApplication1/SoftwareThemeDesigner/MainWindow.xaml.cs
@@ -1,25 +1,37 @@
 ﻿using System;
+using System.Windows.Input;
 using DevExpress.Xpf.Core;
 using SoftwareThemeDesigner.RoutedEvents;
 using SoftwareThemeDesigner.ViewModels;
 
 namespace SoftwareThemeDesigner
 {
-    public partial class MainWindow : ThemedWindow
-    {
-        public MainWindow()
-        {
-            var viewModel = new MainWindowViewModel();
-            DataContext = viewModel;
+	public partial class MainWindow : ThemedWindow
+	{
+		public MainWindow()
+		{
+			var viewModel = new MainWindowViewModel();
+			DataContext = viewModel;
 
-            InitializeComponent();
+			InitializeComponent();
 
 			spinBox.spin += onSpin;
-        }
 
-        private void onSpin(object sender, SpinRoutedEventArgs e)
-        {
-	        Console.WriteLine($"Spin Event Handled: Old Value = {e.oldValue}, New Value = {e.newValue}");
-        }
-    }
+			passwordBox.KeyUp += PasswordBoxOnKeyUp;
+		}
+
+		private void onSpin(object sender, SpinRoutedEventArgs e)
+		{
+			Console.WriteLine($"Spin Event Handled: Old Value = {e.oldValue}, New Value = {e.newValue}");
+		}
+
+		private void PasswordBoxOnKeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter || e.Key == Key.Return)
+			{
+				Console.WriteLine($"Typed Password: {passwordBox.password}");
+				passwordBox.clear();
+			}
+		}
+	}
 }
---
+++ DXApplication1/SoftwareThemeDesigner/RctPasswordBox.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SoftwareThemeDesigner
+{
+	public class RctPasswordBox : RctTextBox
+	{
+		#region Fields
+		private PasswordBox _passwordBox;
+		#endregion
+
+		#region Dependency Properties
+		public static readonly DependencyProperty passwordProperty = DependencyProperty.Register(nameof(password), typeof(string), typeof(RctPasswordBox), new PropertyMetadata(""));
+		#endregion
+
+		#region Properties
+		public string password
+		{
+			get { return (string)GetValue(passwordProperty); }
+			set { SetValue(passwordProperty, value); }
+		}
+		#endregion
+
+		#region Statics
+		static RctPasswordBox()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(RctPasswordBox), new FrameworkPropertyMetadata(typeof(RctPasswordBox)));
+			labelTextProperty.AddOwner(typeof(PasswordBox));
+			labelFontSizeProperty.AddOwner(typeof(PasswordBox));
+			labelTextColorProperty.AddOwner(typeof(PasswordBox));
+		}
+		#endregion
+
+		#region Methods
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			_passwordBox = (PasswordBox)GetTemplateChild("PART_PasswordBox");
+			if (_passwordBox != null)
+			{
+				// Forwards text from internal password box to current password box.
+				_passwordBox.PasswordChanged += (s, e) => password = _passwordBox.Password;
+			}
+		}
+
+		public void clear()
+		{
+			_passwordBox.Clear();
+		}
+		#endregion
+	}
+}

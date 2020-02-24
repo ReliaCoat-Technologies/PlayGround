8f7c78858c139229ce4a8f5aa871e0e13a082e94
Ari <DonSagiv@users.noreply.github.com>
2020-02-24 10:52:10 -0500

    Add List Box (No search functionality)

---
+++ DXApplication1/SoftwareThemeDesigner/RctListBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SoftwareThemeDesigner
+{
+	public class RctListBox : ListBox
+	{
+		static RctListBox()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(RctListBox), new FrameworkPropertyMetadata(typeof(RctListBox)));
+		}
+	}
+}

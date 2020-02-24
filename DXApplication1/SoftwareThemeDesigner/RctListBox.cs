d85ee3b984daba8c8e5ac4f6b816dda11485999e
Ari <DonSagiv@users.noreply.github.com>
2020-02-24 15:22:08 -0500

    Add improved handler for ListBox, start creation of Button.

--- DXApplication1/SoftwareThemeDesigner/RctListBox.cs
+++ DXApplication1/SoftwareThemeDesigner/RctListBox.cs
@@ -29,24 +29,53 @@ namespace SoftwareThemeDesigner
 			searchTextBox = GetTemplateChild("PART_SearchTextBox") as TextBox;
 			if (searchTextBox != null)
 			{
+				searchTextBox.PreviewKeyDown += SearchTextBoxOnPreviewKeyDown;
 				searchTextBox.TextChanged += onSearchBoxTextChanged;
 			}
+		}
+
+		private void SearchTextBoxOnPreviewKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key != Key.Tab) return;
+
+			SelectedItem = hostPanel.Children.OfType<ListBoxItem>()
+				.FirstOrDefault(x => x.IsVisible)
+				?.Content;
+
+			Focus();
+
+			e.Handled = true;
 		}
 
 		protected override void OnPreviewKeyDown(KeyEventArgs e)
 		{
 			if (e.Key == Key.Enter || e.Key == Key.Return)
 			{
-				return;
+				acceptValue();
 			}
 			if (e.Key == Key.Up || e.Key == Key.Down)
 			{
 				return;
 			}
-
+			
 			searchTextBox.Focus();
 
 			base.OnPreviewKeyDown(e);
+		}
+
+		private void acceptValue()
+		{
+			var filteredChildren = hostPanel
+				.Children
+				.OfType<ComboBoxItem>()
+				.Select((x, i) => new { index = i, cbItem = x })
+				.Where(x => x.cbItem.IsVisible)
+				.ToList();
+
+			var firstHighlightedIndex = filteredChildren.FirstOrDefault(x => x.cbItem.IsHighlighted)?.index ?? -1;
+
+			if (!string.IsNullOrWhiteSpace(searchTextBox.Text) && filteredChildren.Count > 0)
+				SelectedIndex = firstHighlightedIndex > 0 ? firstHighlightedIndex : filteredChildren.First().index;
 		}
 
 		private void onSearchBoxTextChanged(object sender, TextChangedEventArgs e)

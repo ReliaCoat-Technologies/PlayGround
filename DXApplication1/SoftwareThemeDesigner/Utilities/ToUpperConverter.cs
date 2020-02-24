e66165828748b123c7a554e03b3458c948c49086
Ari <DonSagiv@users.noreply.github.com>
2020-02-24 17:23:38 -0500

    Add corner radius, all caps text to button.

---
+++ DXApplication1/SoftwareThemeDesigner/Utilities/ToUpperConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using DevExpress.Printing.Core.PdfExport.Metafile;
+
+namespace SoftwareThemeDesigner.Utilities
+{
+	public class ToUpperConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var stringValue = value as string;
+			return stringValue?.ToUpper() ?? value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

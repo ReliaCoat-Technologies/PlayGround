f693431b3fd032c915aeda50d6649876f635d1dd
Ari <DonSagiv@users.noreply.github.com>
2020-02-28 10:18:13 -0500

    Change checkbox color when checked.

--- DXApplication1/SoftwareThemeDesigner/RctCheckBox.cs
+++ DXApplication1/SoftwareThemeDesigner/RctCheckBox.cs
@@ -11,7 +11,7 @@ namespace SoftwareThemeDesigner
 	{
 		#region Constants
 		private const string uncheckedPathData = "M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z";
-		private const string checkedPathData = "M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,5V19H5V5H19M10,17L6,13L7.41,11.58L10,14.17L16.59,7.58L18,9";
+		private const string checkedPathData = "M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z";
 		private const string indeterminatePathData = "M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V5H19V19M17,17H7V7H17V17Z";
 		#endregion
 

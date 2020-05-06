29dd888242a21e1504db8da957c2fc5900996bc2
Ari <DonSagiv@users.noreply.github.com>
2020-05-06 08:25:45 -0400

    Update ComboBox Selection Mechanism

--- DXApplication1/SoftwareThemeDesigner/RctComboBox.cs
+++ DXApplication1/SoftwareThemeDesigner/RctComboBox.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Specialized;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,8 +17,9 @@ namespace SoftwareThemeDesigner
 	public class RctComboBox : ComboBox
 	{
 		#region Fields
+		private readonly IList<ComboBoxItem> _allComboBoxItems;
+		private readonly IDictionary<int, ComboBoxItem> _visibleComboBoxItems;
 		private Popup _popup;
-		private TextBlock _label;
 		private TextBox _searchTextBox;
 		private TextBox _editableTextBox;
 		private ItemsPresenter _hostPanel;
@@ -56,6 +57,7 @@ namespace SoftwareThemeDesigner
 			get { return (bool)GetValue(allowDeleteProperty); }
 			set { SetValue(allowDeleteProperty, value); }
 		}
+		protected int currentIndex => _visibleComboBoxItems.FirstOrDefault(x => x.Value.DataContext == SelectedItem).Key;
 		#endregion
 
 		#region Constructor
@@ -63,8 +65,12 @@ namespace SoftwareThemeDesigner
 		{
 			DefaultStyleKeyProperty.OverrideMetadata(typeof(RctComboBox), new FrameworkPropertyMetadata(typeof(RctComboBox)));
 			FontSizeProperty.OverrideMetadata(typeof(RctComboBox), new FrameworkPropertyMetadata(24d));
+		}
 
-			
+		public RctComboBox()
+		{
+			_allComboBoxItems = new List<ComboBoxItem>();
+			_visibleComboBoxItems = new Dictionary<int, ComboBoxItem>();
 		}
 		#endregion
 
@@ -113,6 +119,12 @@ namespace SoftwareThemeDesigner
 				GroupStyle.Add(defaultGroupStyle);
 		}
 
+		protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+		{
+			_allComboBoxItems?.Clear();
+			_visibleComboBoxItems?.Clear();
+		}
+
 		protected override void OnPreviewKeyDown(KeyEventArgs e)
 		{
 			if (e.Key == Key.Escape)
@@ -139,7 +151,25 @@ namespace SoftwareThemeDesigner
 			}
 			if (e.Key == Key.Up || e.Key == Key.Down)
 			{
-				IsDropDownOpen = true;
+				if (!IsDropDownOpen)
+				{
+					IsDropDownOpen = true;
+				}
+				else if (_searchTextBox.IsFocused)
+				{
+					var firstItem = _visibleComboBoxItems.FirstOrDefault().Value;
+
+					firstItem?.Focus();
+					SelectedItem = firstItem?.DataContext;
+				}
+				else
+				{
+					if (e.Key == Key.Up)
+						goToPreviousSelection();
+					else
+						goToNextSelection();
+				}
+
 				return;
 			}
 
@@ -148,17 +178,46 @@ namespace SoftwareThemeDesigner
 			base.OnPreviewKeyDown(e);
 		}
 
-		private void onSearchBoxTextChanged(object sender, TextChangedEventArgs e)
+		private void goToPreviousSelection()
 		{
-			var children = _hostPanel.VisualChildren().ToList();
-
-			foreach (var item in children.OfType<ComboBoxItem>())
+			if (SelectedItem == null)
 			{
-				var isAvailable = item.Content.containsFilterInOrder(_searchTextBox.Text, DisplayMemberPath);
-				item.Visibility = isAvailable ? Visibility.Visible : Visibility.Collapsed;
+				SelectedIndex = _visibleComboBoxItems.LastOrDefault().Key;
+				return;
 			}
 
-			foreach (var groupItem in children.OfType<GroupItem>())
+			var previous = _visibleComboBoxItems.LastOrDefault(x => x.Key < currentIndex);
+			SelectedIndex = previous.Value == null ? _visibleComboBoxItems.LastOrDefault().Key : previous.Key;
+		}
+
+		private void goToNextSelection()
+		{
+			if (SelectedItem == null)
+			{
+				SelectedIndex = _visibleComboBoxItems.FirstOrDefault().Key;
+				return;
+			}
+
+			var next = _visibleComboBoxItems.FirstOrDefault(x => x.Key > currentIndex);
+			SelectedIndex = next.Value == null ? _visibleComboBoxItems.FirstOrDefault().Key : next.Key;
+		}
+
+		private void onSearchBoxTextChanged(object sender, TextChangedEventArgs e)
+		{
+			_visibleComboBoxItems.Clear();
+
+			for (var i = 0; i < _allComboBoxItems.Count; i++)
+			{
+				var isAvailable = _allComboBoxItems[i].DataContext.containsFilterInOrder(_searchTextBox.Text, DisplayMemberPath);
+				_allComboBoxItems[i].Visibility = isAvailable ? Visibility.Visible : Visibility.Collapsed;
+
+				if (isAvailable)
+					_visibleComboBoxItems.Add(i, _allComboBoxItems[i]);
+			}
+
+			var children = _hostPanel.VisualChildren().OfType<GroupItem>().ToList();
+
+			foreach (var groupItem in children)
 			{
 				var hasVisibleChildren = groupItem
 					.VisualChildren()
@@ -187,11 +246,42 @@ namespace SoftwareThemeDesigner
 		private void onPopupOpened(object sender, EventArgs e)
 		{
 			_searchTextBox.Text = string.Empty;
+
+			var items = _hostPanel.VisualChildren().OfType<ComboBoxItem>().ToList();
+
+			for (var i = 0; i < items.Count; i++)
+			{
+				var item = items[i];
+
+				if (_allComboBoxItems.Contains(item)) continue;
+
+				item.MouseMove += onItemMouseEnter;
+
+				_allComboBoxItems.Add(item);
+				_visibleComboBoxItems.Add(i, item);
+			}
+		}
+
+		private void onItemMouseEnter(object sender, MouseEventArgs e)
+		{
+			var comboBoxItem = sender as ComboBoxItem;
+
+			if (comboBoxItem == null) return;
+
+			SelectedItem = comboBoxItem.DataContext;
 		}
 
 		protected override void OnSelectionChanged(SelectionChangedEventArgs e)
 		{
 			base.OnSelectionChanged(e);
+
+			foreach (var item in _allComboBoxItems.Where(x => x.Background == Brushes.Red))
+				item.Background = Brushes.Transparent;
+
+			var selectedComboBoxItem = _allComboBoxItems.FirstOrDefault(x => x.DataContext == SelectedItem);
+
+			if (selectedComboBoxItem != null)
+				selectedComboBoxItem.Background = Brushes.Red;
 
 			if (_editableTextBox == null) return;
 

44ff9bd831232b87639b5247944fa84779abd343
Ari <DonSagiv@users.noreply.github.com>
2021-02-12 13:48:58 -0500

    Custom-define adorners.

--- SoftwareThemeDesignerTester/ThemeTesterWindow.xaml.cs
+++ SoftwareThemeDesignerTester/ThemeTesterWindow.xaml.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Shapes;
-using ReliaCoat.Common.UI;
 using SoftwareThemeDesignerTester.ViewModels;
 using SoftwareThemeDesignerTester.Views;
 

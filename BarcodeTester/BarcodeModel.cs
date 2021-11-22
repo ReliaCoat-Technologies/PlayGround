f7141b55bf40ec6cc438f8d7ac6ccd97b493f698
Ari <DonSagiv@users.noreply.github.com>
2021-11-22 08:37:52 -0500

    Add barcode generator.

---
+++ BarcodeTester/BarcodeModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using MongoDB.Bson;
+using ZXing;
+
+namespace  BarcodeTester
+{
+	public static class BarcodeModel
+	{
+		public static Bitmap generateBarcode(BarcodeFormat barcodeFormat)
+		{
+			var writer = new BarcodeWriter {Format = barcodeFormat};
+
+			var objectId = ObjectId.GenerateNewId().ToString();
+
+			var barcode = writer.Encode(objectId);
+
+			var renderer = writer.Renderer;
+
+			var bitmap = renderer.Render(barcode, BarcodeFormat.CODE_39, objectId);
+
+			return bitmap;
+		}
+	}
+}
---
+++ BarcodeTester/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using ReliaCoat.Common;
+using ReliaCoat.Common.UI.ValueConverters;
+using ZXing;
+
+namespace BarcodeTester
+{
+	public partial class MainWindow : Window
+	{
+		private readonly IDictionary<string, BarcodeFormat> _comboBoxItems;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			_comboBoxItems = EnumOperations.getEnumDescriptions<BarcodeFormat>();
+
+			var keys = _comboBoxItems.Keys;
+
+			foreach (var key in keys)
+			{
+				formatComboBox.Items.Add(key);
+			}
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			if (formatComboBox.SelectedItem == null)
+			{
+				return;
+			}
+
+			var format = _comboBoxItems[formatComboBox.SelectedItem.ToString()];
+
+			try
+			{
+				var barcodeBitmap = BarcodeModel.generateBarcode(format);
+
+				BarcodeImageHolder.Source = DrawingToMediaConverter.getBitmapImage(barcodeBitmap);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
+}

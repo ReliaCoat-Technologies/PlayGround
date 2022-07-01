3dd410c65148546b6367fef1edde93e66a7d97ce
Ari <DonSagiv@users.noreply.github.com>
2022-07-01 11:25:27 -0400

    Test Box-Benhken Design

--- DesignOfExperiments/Program.cs
+++ DesignOfExperiments/Program.cs
@@ -8,6 +8,29 @@ namespace DesignOfExperiments
     {
         static void Main(string[] args)
         {
+            // createFactorialDesign();
+
+            var numParameters = 3;
+
+            var parameters = Enumerable.Range(0, numParameters)
+                .Select(x => new DesignParameter<double>(x.ToString(), -1, 0, 1))
+                .ToArray();
+
+            var doe = BoxBenkhenDesignOfExperimentsCreator.designExperiment(false, parameters);
+
+            Console.WriteLine($"Number of Parameters: {doe.numParameters}");
+            Console.WriteLine($"Number of Experiments: {doe.numExperiments}");
+
+            foreach (var experiment in doe.experimentConditions)
+            {
+                Console.WriteLine(experiment.ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void createFactorialDesign()
+        {
             var numParameters = 6;
             var sizeOfFraction = 2;
 
